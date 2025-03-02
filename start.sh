674b3acc1c8ad132a94b30c6360d4c0a415d0fde
Rodrigom33 <seu.email@exemplo.com>
2025-03-02 00:46:03 -0300

    Ajustando script de inicialização

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Aplica as migrações do banco de dados
+python manage.py migrate --no-input
+
 # Coleta arquivos estáticos
 python manage.py collectstatic --no-input
 
 # Inicia o servidor
-gunicorn mysite.wsgi 
+gunicorn mysite.wsgi --log-file - 
