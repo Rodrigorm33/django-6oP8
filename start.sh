90273cea9a66835bf2539564b7f2206beaa1e281
Rodrigom33 <seu.email@exemplo.com>
2025-03-01 21:56:18 -0300

    Removendo importação automática desnecessária

--- start.sh
+++ start.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-# Executar migrações apenas para outras apps do Django
-echo "Iniciando migrações..."
-python manage.py migrate auth
-python manage.py migrate admin
-python manage.py migrate sessions
-python manage.py migrate contenttypes
+# Coleta arquivos estáticos
+python manage.py collectstatic --no-input
 
-# Coletar arquivos estáticos
-echo "Coletando arquivos estáticos..."
-python manage.py collectstatic --noinput
-
-# Tentar importar dados
-echo "Tentando importar dados..."
-python manage.py importar_multas
-
-# Iniciar o servidor Gunicorn
-echo "Iniciando o servidor..."
-gunicorn mysite.wsgi:application --bind 0.0.0.0:8000 
+# Inicia o servidor
+gunicorn mysite.wsgi 
