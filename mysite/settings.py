e6d847d71c1acfbff4127bb88cb34654a213c615
Rodrigom33 <seu.email@exemplo.com>
2025-03-01 23:26:48 -0300

    Ajustando configurações de arquivos estáticos para evitar duplicação

--- mysite/settings.py
+++ mysite/settings.py
@@ -110,9 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "multas/static"),
-]
+STATICFILES_DIRS = []
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # WhiteNoise Configuration
