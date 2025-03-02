15f866e23744fd8d7a296ab34f899f9c013f5107
Rodrigom33 <seu.email@exemplo.com>
2025-03-02 00:49:26 -0300

    Resolvendo conflitos no settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,20 +16,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-<<<<<<< HEAD
 SECRET_KEY = os.environ.get('SECRET_KEY')
-=======
-SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk'
->>>>>>> b4030ba220eb8d341c62a99b4020f00e2ed0cbf5
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-<<<<<<< HEAD
 ALLOWED_HOSTS = ['django-6op8.up.railway.app', '.railway.app']
-=======
-ALLOWED_HOSTS = ["*"]
->>>>>>> b4030ba220eb8d341c62a99b4020f00e2ed0cbf5
 
 # Application definition
 
@@ -40,21 +32,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
     'multas.apps.MultasConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-=======
-    'multas',
-]
-
-MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.security.SecurityMiddleware',
->>>>>>> b4030ba220eb8d341c62a99b4020f00e2ed0cbf5
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -68,11 +51,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-<<<<<<< HEAD
         'DIRS': [],
-=======
-        'DIRS': [os.path.join(BASE_DIR, 'mysite/templates')],
->>>>>>> b4030ba220eb8d341c62a99b4020f00e2ed0cbf5
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -92,21 +71,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-<<<<<<< HEAD
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('PGDATABASE'),
         'USER': os.environ.get('PGUSER'),
         'PASSWORD': os.environ.get('PGPASSWORD'),
         'HOST': os.environ.get('PGHOST'),
         'PORT': os.environ.get('PGPORT'),
-=======
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
->>>>>>> b4030ba220eb8d341c62a99b4020f00e2ed0cbf5
     }
 }
 
@@ -131,19 +101,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-<<<<<<< HEAD
 LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'America/Sao_Paulo'
-=======
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
->>>>>>> b4030ba220eb8d341c62a99b4020f00e2ed0cbf5
 USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-<<<<<<< HEAD
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
@@ -161,18 +125,6 @@ SECURE_HSTS_PRELOAD = True
 WHITENOISE_USE_FINDERS = True
 WHITENOISE_MANIFEST_STRICT = False
 WHITENOISE_ALLOW_ALL_ORIGINS = True
-=======
-STATICFILES_DIRS = []  # Removendo diretórios adicionais para evitar duplicação
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-# WhiteNoise Configuration
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-WHITENOISE_USE_FINDERS = True
-WHITENOISE_MANIFEST_STRICT = False
-WHITENOISE_ALLOW_ALL_ORIGINS = True
-WHITENOISE_INDEX_FILE = True
-WHITENOISE_AUTOREFRESH = True
->>>>>>> b4030ba220eb8d341c62a99b4020f00e2ed0cbf5
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
