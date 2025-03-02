085ed1bfd963b62f1c77718b7ce66b1657bb7fd4
Rodrigom33 <seu.email@exemplo.com>
2025-03-01 21:24:59 -0300

    Adicionando migração e importação automática durante deploy

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
+import subprocess
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+# Executar migrações
+subprocess.run(['python', 'manage.py', 'migrate'])
+
+# Executar importação de dados
+subprocess.run(['python', 'manage.py', 'importar_multas'])
 
 application = get_wsgi_application()
